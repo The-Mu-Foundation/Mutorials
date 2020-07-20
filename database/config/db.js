fbf2dac0ee4933e0515562d26543f81b3a0f8a2c
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2020-07-20 16:21:41 -0700

    smh node, join the cool club
    seriously!?

--- database/config/db.js
+++ database/config/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 
-const MONGOURI = process.env.DB_URL ?? "mongodb+srv://Skippy:PNSwim19@cluster0-7gibd.mongodb.net/test?retryWrites=true&w=majority"
+const MONGOURI = process.env.DB_URL || "mongodb+srv://Skippy:PNSwim19@cluster0-7gibd.mongodb.net/test?retryWrites=true&w=majority"
 // Test database URI: "mongodb+srv://Skippy:PNSwim19@cluster0-7gibd.mongodb.net/test?retryWrites=true&w=majority"
 // Public database URI: "mongodb+srv://Skippy:PNSwim19@cluster0-7gibd.mongodb.net/mutorialsone?retryWrites=true&w=majority"
 
