5ee7789bb99ca04f933e2fa185f000c0531f6e80
Michael <michael.mingdao.li@gmail.com>
2020-10-01 23:29:04 -0700

    SITE CRASHED QUICK FIX

--- routes/public.js
+++ routes/public.js
@@ -88,7 +88,8 @@ module.exports = (app, mongo) => {
             if (!Boolean(req.body['h-captcha-response'])) {
                 req.flash('errorFlash', 'Invalid captcha.');
                 registerInputProblems1 = true;
-
+            }
+        });
         if (req.body.ign.length < 1) {
             req.flash('errorFlash', 'Please enter a username.');
             registerInputProblems1 = true;
@@ -122,6 +123,7 @@ module.exports = (app, mongo) => {
             res.redirect('/signup');
             return; // to prevent ERRHTTPHEADERSSENT
         }
+        
 
         const saltHash = genPassword(req.body.password);
 
@@ -148,101 +150,44 @@ module.exports = (app, mongo) => {
                 physics: -1,
                 chemistry: -1,
                 biology: -1
-
             }
-            if (req.body.ign.length < 1) {
-                req.flash('errorFlash', 'Please enter a username.');
-                registerInputProblems1 = true;
-            }
-            if (req.body.password.length < 7 || !(/\d/.test(req.body.password)) || !(/[a-zA-Z]/.test(req.body.password))) {
-                req.flash('errorFlash', 'The password you entered does not meet the requirements.');
-                registerInputProblems1 = true;
-            }
-            if (!emailValidation.regexCheck(req.body.username)) {
-                console.log(req.body.username);
-                req.flash('errorFlash', 'The email you entered is not valid.');
-                registerInputProblems1 = true;
-            }
-            if (!req.body.agreeTOS) {
-                req.flash('errorFlash', 'You must agree to the Terms of Service and Privacy Policy to register an account with us.');
-                registerInputProblems1 = true;
-            }
-            if (!req.body.agreeAge) {
-                req.flash('errorFlash', 'You must be at least 13 years old, or have permission from your parent, guardian, teacher, or school to use Mutorials.');
-                registerInputProblems1 = true;
-            }
-            if (!(/^\d+$/.test(req.body.age))) {
-                req.flash('errorFlash', 'Please enter a valid age!');
-                registerInputProblems1 = true;
-            }
-            if (registerInputProblems1) {
-                res.redirect('/signup');
-                return; // to prevent ERRHTTPHEADERSSENT
-            }
-
-            const saltHash = genPassword(req.body.password);
-
-            const salt = saltHash.salt;
-            const hash = saltHash.hash;
-            const newUser = new mongo.User({
-                username: req.body.username,
-                ign: req.body.ign,
-                hash: hash,
-                salt: salt,
-                profile: {
-                    name: '',
-                    location: 'Earth',
-                    age: req.body.age,
-                    bio: ''
-                },
-                // if emailConfirmCode == 0, then email is confirmed
-                stats: {
-                    correct: 0,
-                    wrong: 0,
-                    collectedTags: []
-                },
-                rating: {
-                    physics: -1,
-                    chemistry: -1,
-                    biology: -1
-                }
-            });
-            // check for duplicate user
-            mongo.db.collection('users').findOne({ username: req.body.username }).then((user) => {
-                if (user) {
-                    console.log('used');
-                    var registerInputProblems2 = false;
-                    if (user.ign == req.body.ign) {
-                        req.flash('errorFlash', 'This username is already taken.');
-                        registerInputProblems2 = true;
-                    } else { // has to be matching email
-                        req.flash('errorFlash', 'This email is already in use.');
-                        registerInputProblems2 = true;
-                    }
-                    if (registerInputProblems2) {
-                        res.redirect('/signup');
-                        return; // to prevent ERRHTTPHEADERSSENT
-                    }
-                } else {
-                    console.log('new one');
-                    newUser.save()
-                        .then((user) => {
-                            //passport.authenticate('local', {failureRedirect: '/signin', successRedirect: '/train'});
-                            console.log(user);
-                        });
-                    req.flash('successFlash', 'We successfully signed you up!');
-                    var confirmCode;
-                    require('crypto').randomBytes(6, function (ex, buf) {
-                        confirmCode = buf.toString('hex');
-                        mongo.db.collection('users').findOneAndUpdate({ username: req.body.username }, { $set: { emailConfirmCode: confirmCode } });
-                        emailValidation.emailCodeSend(req.body.username, confirmCode);
-                        req.flash('errorFlash', 'You need to confirm your email. Please check your email for instructions.');
-                    });
-                }
-                res.redirect('/signin');
-            });
         });
+        // check for duplicate user
+        mongo.db.collection('users').findOne({ username: req.body.username }).then((user) => {
+            if (user) {
+                console.log('used');
+                var registerInputProblems2 = false;
+                if (user.ign == req.body.ign) {
+                    req.flash('errorFlash', 'This username is already taken.');
+                    registerInputProblems2 = true;
+                } else { // has to be matching email
+                    req.flash('errorFlash', 'This email is already in use.');
+                    registerInputProblems2 = true;
+                }
+                if (registerInputProblems2) {
+                    res.redirect('/signup');
+                    return; // to prevent ERRHTTPHEADERSSENT
+                }
+            } else {
+                console.log('new one');
+                newUser.save().then((user) => {
+                    //passport.authenticate('local', {failureRedirect: '/signin', successRedirect: '/train'});
+                    console.log(user);
+                });
+                req.flash('successFlash', 'We successfully signed you up!');
+                var confirmCode;
+                require('crypto').randomBytes(6, function (ex, buf) {
+                    confirmCode = buf.toString('hex');
+                    mongo.db.collection('users').findOneAndUpdate({ username: req.body.username }, { $set: { emailConfirmCode: confirmCode } });
+                    emailValidation.emailCodeSend(req.body.username, confirmCode);
+                    req.flash('errorFlash', 'You need to confirm your email. Please check your email for instructions.');
+                });
+            }
+            res.redirect('/signin');
+        });
+        
     });
+
     app.post('/login', passport.authenticate('local', {
         failureRedirect: '/signin',
         successRedirect: '/homepage',
