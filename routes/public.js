fad00bfd769456351f58def3075f20bfadc5b486
Ethan Shen <45775339+Skipppyyy@users.noreply.github.com>
2021-02-21 21:16:26 -0800

    Quick typo fixes  (#358)
    * fixed check on year of birth for registering
    
    * Text changes & Stats typo fix
    
    * formatting typo error
    
    * another formatting typo
    
    Co-authored-by: Michael Li <45986807+ML72@users.noreply.github.com>

--- routes/public.js
+++ routes/public.js
@@ -117,7 +117,7 @@ module.exports = (app, mongo) => {
             req.flash('errorFlash', 'You must be at least 13 years old, or have permission from your parent, guardian, teacher, or school to use Mutorials.');
             registerInputProblems1 = true;
         }
-
+        
         if (!(/^(19|20)\d{2}$/.test(req.body.yob)) || req.body.yob.length != 4 || req.body.yob > new Date().getFullYear()) {
             req.flash('errorFlash', 'Please enter a valid year of birth!');
             registerInputProblems1 = true;
