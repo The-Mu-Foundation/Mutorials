5e4c71070fac7bd9b2f2a6c641403a872d8f8b5a
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2021-03-02 12:37:11 -0800

    production

--- utils/functions/webhook.js
+++ utils/functions/webhook.js
@@ -25,7 +25,8 @@ module.exports = {
                 }
             ]
         };
-        fetch('https://discord.com/api/webhooks/809235980883066891/Te4OwCbAwoXUfKThSndr-TdUzfiONjnA83-6J2mVEGaLKNHIPLlYFYuawNAg9G7XEGZF', {
+        const url = process.env.DISCORD_WEBHOOK_URL || 'https://discord.com/api/webhooks/809235980883066891/Te4OwCbAwoXUfKThSndr-TdUzfiONjnA83-6J2mVEGaLKNHIPLlYFYuawNAg9G7XEGZF';
+        fetch(url, {
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(params)
