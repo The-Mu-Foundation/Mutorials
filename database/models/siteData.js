d56e8c329ab80b6544dec018832a81ee585206ed
TozyCo <106439361+TozyCo@users.noreply.github.com>
2022-05-28 07:32:59 -0700

    Delete database/models directory

--- database/models/siteData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-// define user schema
-const siteDataSchema = new mongoose.Schema({
-    tag: String,
-    data: Object
-});
-
-module.exports = { siteDataSchema };
