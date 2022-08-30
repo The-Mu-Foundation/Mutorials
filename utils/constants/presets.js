4fb7fbf6bb05c7728bd2fac79c50b80034a633d6
Daniel L <63090804+dannyridel@users.noreply.github.com>
2022-08-30 15:01:24 -0700

    Update presets.js

--- utils/constants/presets.js
+++ utils/constants/presets.js
@@ -1,17 +1,17 @@
 // fill in preset units later
 const presetUnitOptions = {
     "Physics": {
-        "AP Physics 1": ["Kinematics", "Forces", "Rotational Motion", "Energy", "Momentum and Collisions", "Gravitation", "Simple Harmonic Motion"],
-        "AP Physics 2": ["Energy", "Solids and Fluids", "Thermodynamics", "Electric Forces and Fields", "Current and Resistance", "DC Circuits", "Magnetism", "Inductance", "Vibrations and Waves", "Geometric Optics", "Physical Optics", "Quantum Physics", "Nuclear Energy and Elementary Particles", "Electrostatics"],
-        "AP Physics C Mechanics": ["Kinematics", "Forces", "Energy", "Momentum and Collisions", "Rotational Motion", "Simple Harmonic Motion"],
-        "AP Physics C EM": ["Electrostatics", "Electrical Energy and Capacitance", "Current and Resistance", "DC Circuits", "Magnetism", "Inductance"],
-        "IB Physics SL2": ["Kinematics", "Forces", "Energy", "Momentum and Collisions", "Thermodynamics", "Simple Harmonic Motion", "Vibrations and Waves", "Reflection and Refraction", "Wave Optics", "Electric Forces and Fields", "Current and Resistance", "DC Circuits", "AC Circuits", "Magnetism", "Rotational Motion", "Nuclear Energy and Elementary Particles", "Quantum Physics", "Relativity"],
-        "IB Physics HL1": ["Kinematics", "Forces", "Energy", "Momentum and Collisions", "Thermodynamics", "Simple Harmonic Motion", "Vibrations and Waves", "Reflection and Refraction", "Wave Optics", "Electric Forces and Fields", "Current and Resistance", "DC Circuits", "AC Circuits", "Magnetism", "Rotational Motion", "Nuclear Energy and Elementary Particles", "Quantum Physics", "Inductance", "Relativity", "Solids and Fluids"]
+        "AP Physics 1": [],
+        "AP Physics 2": [],
+        "AP Physics C Mechanics": [],
+        "AP Physics C EM": [],
+        "IB Physics SL1": [],
+        "IB Physics SL2/HL2": [],
+        "F=ma Exam": []
     },
     "Chemistry": {
         "AP Chemistry": ["Atomic Structure", "Bonding", "Gases", "Solubility", "Stoichiometry", "Acids and Bases", "Chemical Kinetics", "Thermochemistry", "Chemical Equilibrium", "Electrochemistry", "Spontaneity, Entropy, and Free Energy", "Chemical Reactions"],
         "IB Chemistry SL2": ["Atomic Structure", "Bonding", "Gases", "Solubility", "Stoichiometry", "Acids and Bases", "Chemical Kinetics", "Thermochemistry", "Chemical Equilibrium", "Electrochemistry", "Spontaneity, Entropy, and Free Energy", "Chemical Reactions", "Organic Chemistry", "Materials Chemistry", "Biochemistry"],
-        "IB Chemistry HL1": ["Atomic Structure", "Bonding", "Gases", "Solubility", "Stoichiometry", "Acids and Bases", "Chemical Kinetics", "Thermochemistry", "Chemical Equilibrium", "Electrochemistry", "Spontaneity, Entropy, and Free Energy", "Chemical Reactions", "Organic Chemistry", "Materials Chemistry", "Biochemistry"]
     },
     "Biology": {
         "AP Biology": ["Molecular Biology","Cell Biology","Metabolism","Heredity and Genetics","Evolution","Ecology"],
