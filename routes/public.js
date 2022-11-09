4e9f16c742e82974a9ec090675a0db79fdeb3499
William Y. Feng <womogenes2@gmail.com>
2022-11-08 17:54:24 -0800

    Update year on copyright notice to 2022 (#469)

--- routes/public.js
+++ routes/public.js
@@ -139,7 +139,7 @@ module.exports = (app, mongo) => {
         const hash = saltHash.hash;
         let thisYob = req.body.yob;
         if (!thisYob) {
-            thisYob = 2020;
+            thisYob = 2022;
         }
         const newUser = new mongo.User({
             username: req.body.username,
