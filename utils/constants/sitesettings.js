b2c4cee8e15ec67709fb15106fd8bfbdc63c0337
DEEZ Nuts <93239187+TheFalloutOf76@users.noreply.github.com>
2023-05-31 22:13:45 -0700

    prevent user from selectively answering questions

--- utils/constants/sitesettings.js
+++
@@ -1,4 +0,0 @@
-const adminList = process.env.adminemails?.split(';') || ['mutorialsproject@gmail.com', 'patrickfengsr@gmail.com', 'testing1@gmail.com', 'womogenes2@gmail.com'];
-const contributorList = [];
-
-module.exports = { adminList: adminList, contributorList: contributorList };
