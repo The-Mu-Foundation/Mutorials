d780334fc55af3011e994b6fa2fd43cefc325880
DEEZ Nuts <93239187+TheFalloutOf76@users.noreply.github.com>
2023-06-02 22:55:29 -0700

    fuck github

---
+++ utils/constants/sitesettings.js
@@ -0,0 +1,4 @@
+const adminList = process.env.adminemails?.split(';') || ['mutorialsproject@gmail.com', 'patrickfengsr@gmail.com', 'testing1@gmail.com', 'womogenes2@gmail.com'];	
+const contributorList = [];	
+
+module.exports = { adminList: adminList, contributorList: contributorList };
