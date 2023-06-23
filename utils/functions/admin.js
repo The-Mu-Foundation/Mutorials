580e1bc194cb474249e21b2bea7731af4781b1d1
Daniel L <63090804+dannyridel@users.noreply.github.com>
2023-06-22 20:28:38 -0700

    Update admin.js

--- utils/functions/admin.js
+++ utils/functions/admin.js
@@ -189,7 +189,7 @@ function calculateHours(subject, rating, hourRefactor) {
     } else if (subject == "Chemistry") {
         return 0.03 * Math.pow(Math.E, 0.001 * rating) * hourRefactor;
     } else if (subject == "Biology" || subject == "ESS") {
-        return 0.14 * Math.pow(Math.E, 0.0004 * rating) * hourRefactor;
+        return 0.14 * Math.pow(Math.E, 0.0005 * rating) * hourRefactor;
     }
 }
 
